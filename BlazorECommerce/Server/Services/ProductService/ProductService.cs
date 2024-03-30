732609abc472abd8fc1b25addb4c4fc36d9abe7e
Iggy988 <igor.rodic1988@gmail.com>
2024-03-30 21:29:26 +0100

    Get single product - server side

--- BlazorECommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorECommerce/Server/Services/ProductService/ProductService.cs
@@ -9,6 +9,7 @@ public class ProductService : IProductService
     {
         _context = context;
     }
+
     public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
     {
         var response = new ServiceResponse<List<Product>>
@@ -17,4 +18,23 @@ public class ProductService : IProductService
         };
         return response;
     }
+
+    public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+    {
+        var response = new ServiceResponse<Product>();
+        var product = await _context.Products.FindAsync(productId);
+        if (product == null)
+        {
+            response.Success = false;
+            response.Message = "Sorry, but this product does not exist.";
+        }
+        else
+        {
+            response.Data = product;
+        }
+
+        return response;
+    }
+
+    
 }
