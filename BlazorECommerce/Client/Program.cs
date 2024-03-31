61f044d07caecf5ba7e4287db94eca1553721870
Iggy988 <igor.rodic1988@gmail.com>
2024-03-31 20:46:50 +0200

    Add CategoryService in Client

--- BlazorECommerce/Client/Program.cs
+++ BlazorECommerce/Client/Program.cs
@@ -1,9 +1,11 @@
 global using BlazorECommerce.Shared;
 global using BlazorECommerce.Client.Services.ProductService;
+global using BlazorECommerce.Client.Services.CategoryService;
 using BlazorECommerce.Client;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 
 await builder.Build().RunAsync();
---
+++ BlazorECommerce/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿
+using BlazorECommerce.Shared;
+using System.Net.Http;
+
+namespace BlazorECommerce.Client.Services.CategoryService;
+
+public class CategoryService : ICategoryService
+{
+    private readonly HttpClient _http;
+
+    public CategoryService(HttpClient http)
+    {
+        _http = http;
+    }
+    public List<Category> Categories { get; set; } = new List<Category>();
+
+    public async Task GetCategories()
+    {
+        var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
+        if (response is not null && response.Data != null)
+            Categories = response.Data;
+    }
+}
