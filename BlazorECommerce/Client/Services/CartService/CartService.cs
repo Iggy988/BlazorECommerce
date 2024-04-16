15bc4cf8c5716b78bc871fb38a9d07f0d2616d06
Iggy988 <igor.rodic1988@gmail.com>
2024-04-16 21:17:42 +0200

    Get the Cart Products on the Client

--- BlazorECommerce/Client/Services/CartService/CartService.cs
+++ BlazorECommerce/Client/Services/CartService/CartService.cs
@@ -1,15 +1,19 @@
 ﻿
 using BlazorECommerce.Shared;
+using BlazorECommerce.Shared.DTO;
+using System.Collections.Generic;
 
 namespace BlazorECommerce.Client.Services.CartService;
 
 public class CartService : ICartService
 {
     private readonly ILocalStorageService _localStorage;
+    private readonly HttpClient _http;
 
-    public CartService(ILocalStorageService localStorage)
+    public CartService(ILocalStorageService localStorage, HttpClient http)
     {
         _localStorage = localStorage;
+        _http = http;
     }
 
     public event Action OnChange;
@@ -35,5 +39,14 @@ public class CartService : ICartService
             cart = new List<CartItem>();
         }
         return cart;
+    }
+
+    public async Task<List<CartProductResponseDTO>> GetCartProducts()
+    {
+        var cartItems = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+        var response = await _http.PostAsJsonAsync("api/cart/products", cartItems);
+        var cartProducts = await response.Content.ReadFromJsonAsync<ServiceResponse<List<CartProductResponseDTO>>>();
+        return cartProducts.Data;
+
     }
 }
--- BlazorECommerce/Client/Services/CartService/ICartService.cs
+++ BlazorECommerce/Client/Services/CartService/ICartService.cs
@@ -1,8 +1,11 @@
-﻿namespace BlazorECommerce.Client.Services.CartService;
+﻿using BlazorECommerce.Shared.DTO;
+
+namespace BlazorECommerce.Client.Services.CartService;
 
 public interface ICartService
 {
     event Action OnChange; // whenever something changes in cart
     Task AddToCart(CartItem cartItem);
     Task<List<CartItem>> GetCartItems();
+    Task<List<CartProductResponseDTO>> GetCartProducts();
 }
