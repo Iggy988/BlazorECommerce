fd14d18f643877716eba83bf92c4d0ff8470b3c7
Iggy988 <igor.rodic1988@gmail.com>
2024-04-18 21:29:02 +0200

    Create UserRegister model

--- BlazorECommerce/Shared/CartItem.cs
+++ BlazorECommerce/Shared/CartItem.cs
@@ -9,5 +9,5 @@ public class CartItem
 {
     public int ProductId { get; set; }
     public int ProductTypeId { get; set; }
-    public int Quantity { get; set; } = 1;
+    public int Quantity { get; set; } = 1; //stavljamo default value 1 da se cart moze povecati kad izbrisemo i didamo item
 }
