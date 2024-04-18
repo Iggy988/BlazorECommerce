e0afb3d7b6f63e3a96fcf28655ca383bed063a05
Iggy988 <igor.rodic1988@gmail.com>
2024-04-18 22:38:11 +0200

    Add an Authentication Service on the Server

--- BlazorECommerce/Server/Program.cs
+++ BlazorECommerce/Server/Program.cs
@@ -4,7 +4,9 @@ global using BlazorECommerce.Server.Data;
 global using BlazorECommerce.Server.Services.ProductService;
 global using BlazorECommerce.Server.Services.CategoryService;
 global using BlazorECommerce.Server.Services.CartService;
+global using BlazorECommerce.Server.Services.AuthService;
 using Microsoft.AspNetCore.ResponseCompression;
+
 
 
 
@@ -27,6 +29,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
