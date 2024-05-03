2e5620ded11547ec0eb4be548f0d10f19d88bab1
Iggy988 <igor.rodic1988@gmail.com>
2024-05-03 16:59:17 +0200

    Adding Order and OrderItems tables

--- BlazorECommerce/Server/Data/DataContext.cs
+++ BlazorECommerce/Server/Data/DataContext.cs
@@ -17,6 +17,9 @@ public class DataContext : DbContext
         modelBuilder.Entity<ProductVariant>()
             .HasKey(x => new { x.ProductId, x.ProductTypeId});
 
+        modelBuilder.Entity<OrderItem>()
+           .HasKey(oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId });
+
         modelBuilder.Entity<ProductType>().HasData(
                    new ProductType { Id = 1, Name = "Default" },
                    new ProductType { Id = 2, Name = "Paperback" },
@@ -278,4 +281,6 @@ public class DataContext : DbContext
     public DbSet<ProductVariant> ProductVariants{ get; set; }
     public DbSet<User> Users{ get; set; }
     public DbSet<CartItem> CartItems{ get; set; }
+    public DbSet<Order> Orders{ get; set; }
+    public DbSet<OrderItem> OrderItems{ get; set; }
 }
